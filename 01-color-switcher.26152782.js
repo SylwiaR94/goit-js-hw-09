c7386ad60e25e65e974d377fecf058476c1cbb2b
SylwiaR94 <135271821+SylwiaR94@users.noreply.github.com>
2024-01-05 21:29:44 +0000

    Deploying to gh-pages from @ SylwiaR94/goit-js-hw-09@1d211bf5b3e37530885e49239efe263ecaf1fb3a 🚀

--- 01-color-switcher.26152782.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]");document.querySelector("[data-stop]");t.addEventListener("click",(function(){let t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}));
-//# sourceMappingURL=01-color-switcher.26152782.js.map
