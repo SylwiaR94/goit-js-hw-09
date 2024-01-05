32ce2e075f76cd94a0537fa555aa9b044635bc04
SylwiaR94 <135271821+SylwiaR94@users.noreply.github.com>
2024-01-05 21:41:43 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,15 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener("click", startColor);
+
+function startColor() {
+
+    let hexColor = getRandomHexColor();
+
+    document.body.style.backgroundColor = hexColor;
+
+}
 
 function getRandomHexColor() {  
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
